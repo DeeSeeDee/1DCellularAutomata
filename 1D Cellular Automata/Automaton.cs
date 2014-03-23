9ad8149174063396588b3941a32e40fe78a8c19d
Dan Dorset <dcdorset@gmail.com>
2014-03-22 23:03:50 -0500

    Initial Commit
    This version is stable, and allows for on-the-fly changing of Automata
    ruleset.

---
+++ 1D Cellular Automata/Automaton.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1D_Cellular_Automata
+{
+    class Automaton
+    {
+
+        public char[] Cells
+        {
+            get { return cells; }
+        }
+        private char[] cells;
+
+        private char[] ruleset;
+
+        public int Size
+        {
+            get { return size; }
+        }
+        private int size = 5;
+
+        public int Generation
+        {
+            get { return generation; }
+        }
+        private int generation = 0;
+
+        public Automaton(int width, int rule)
+        {
+            cells = new char[width / size];
+            string ruleString = ToBin(rule, 8);
+            ruleset = ruleString.ToCharArray();
+            for (int i = 0; i < ruleset.Length; i++)
+            {
+                if (ruleset[i] != '1')
+                {
+                    ruleset[i] = '\0';
+                }
+            }
+            cells[cells.Length / 2] = '1';
+        }
+
+        public void Generate()
+        {
+            char[] nextGen = new char[cells.Length];
+            for (int i = 1; i < cells.Length - 1; i++)
+            {
+                char left = cells[i - 1];
+                char me = cells[i];
+                char right = cells[i + 1];
+                nextGen[i] = Rules(left, me, right);
+            }
+            cells = nextGen;
+            generation++;
+        }
+
+        public static string ToBin(int value, int len)
+        {
+            return (len > 1 ? ToBin(value >> 1, len - 1) : null) + "01"[value & 1];
+        }
+
+        private char Rules(char a, char b, char c)
+        {
+            if (a == '1' && b == '1' && c == '1')
+                return ruleset[0];
+            else if (a == '1' && b == '1' && c == '\0')
+                return ruleset[1];
+            else if (a == '1' && b == '\0' && c == '1')
+                return ruleset[2];
+            else if (a == '1' && b == '\0' && c == '\0')
+                return ruleset[3];
+            else if (a == '\0' && b == '1' && c == '1')
+                return ruleset[4];
+            else if (a == '\0' && b == '1' && c == '\0')
+                return ruleset[5];
+            else if (a == '\0' && b == '\0' && c == '1')
+                return ruleset[6];
+            else if (a == '\0' && b == '\0' && c == '\0')
+                return ruleset[7];
+            return '1';
+        }
+    }
+}
---
+++ 1D Cellular Automata/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Xceed.Wpf.Toolkit;
+
+namespace _1D_Cellular_Automata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Canvas canvas;
+        Automaton automaton;
+        DispatcherTimer timer;
+        IntegerUpDown numberControl;
+        DrawWindow drawWindow;
+        int xPointer;
+        int yPointer;
+        int squareSize;
+
+        public MainWindow()
+        {
+            drawWindow = new DrawWindow();
+            InitializeComponent();
+            numberControl = new IntegerUpDown();
+            numberControl.AllowSpin = true;
+            numberControl.Maximum = 256;
+            numberControl.Minimum = 0;
+            numberControl.AllowSpin = true;
+            numberControl.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            numberControl.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            numberControl.Margin = new Thickness(45, 23, 0, 0);
+            numberControl.Value = 1;
+            this.mainGrid.Children.Add(numberControl);
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            timer.Tick += TimerTick;
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (CheckMaxHeight())
+            {
+                return;
+            }
+            DrawRow();
+            timer.Start();
+        }
+
+        private bool CheckMaxHeight()
+        {
+            if (this.yPointer >= this.canvas.Height)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void DrawRow()
+        {
+            xPointer = 0;
+            while ( xPointer < (canvas.Width / squareSize) )
+            {
+                char[] currentCells = automaton.Cells;
+                Rectangle rectangle = new Rectangle();
+                rectangle.Width = rectangle.Height = squareSize;
+                if (currentCells[xPointer] == '1')
+                {
+                    rectangle.Fill = new SolidColorBrush(Colors.Azure);
+                }
+                else
+                {
+                    rectangle.Fill = new SolidColorBrush(Colors.Peru);
+                }
+                Canvas.SetLeft(rectangle, xPointer * squareSize);
+                Canvas.SetTop(rectangle, yPointer);
+                canvas.Children.Add(rectangle);
+                xPointer++;
+            }
+            yPointer += squareSize;
+            automaton.Generate();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.yPointer = 0;
+            drawWindow.Content = null;
+            int caNum = (int)numberControl.Value;
+            canvas = new Canvas();
+            canvas.Width = 550;
+            canvas.Height = 600;
+            canvas.Background = new SolidColorBrush(Colors.White);
+            Canvas.SetTop(canvas, 20);
+            Canvas.SetLeft(canvas, 20);
+            drawWindow.Content = canvas;
+
+            automaton = new Automaton((int)canvas.Width, caNum);
+            squareSize = automaton.Size;
+
+            drawWindow.Show();
+            timer.Start();
+        }
+    }
+}
